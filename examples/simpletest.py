36888f86a1145d1b0f7922999d65287ed83e31e7
Tony DiCola <tony@tonydicola.com>
2017-12-01 15:54:52 -0800

    Update simpletest.py

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -10,7 +10,7 @@ import adafruit_max31865
 
 # Initialize SPI bus and sensor.
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-cs =  digitalio.DigitalInOut(board.D5)  # Chip select of the MAX31865 board.
+cs  = digitalio.DigitalInOut(board.D5)  # Chip select of the MAX31865 board.
 sensor = adafruit_max31865.MAX31865(spi, cs)
 # Note you can optionally provide the thermocouple RTD nominal, the reference
 # resistance, and the number of wires for the sensor (2 the default, 3, or 4)
