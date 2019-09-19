c3dd04c7646321a5572790e38490dda7588ca4bf
Kattni Rembor <kattni@adafruit.com>
2019-09-19 17:56:43 -0400

    Fixes #12

--- adafruit_max31865.py
+++ adafruit_max31865.py
@@ -55,6 +55,8 @@ from micropython import const
 
 import adafruit_bus_device.spi_device as spi_device
 
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MAX31865.git"
 
 #pylint: disable=bad-whitespace
 # Register and other constant values:
