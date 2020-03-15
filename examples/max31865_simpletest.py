c2c1208eb4998ad26069bc5592ff98eb9d1503c4
dherrada <dylan.herrada@gmail.com>
2020-03-15 18:41:20 -0400

    Ran black, updated to pylint 2.x

--- examples/max31865_simpletest.py
+++ examples/max31865_simpletest.py
@@ -16,13 +16,13 @@ sensor = adafruit_max31865.MAX31865(spi, cs)
 # Note you can optionally provide the thermocouple RTD nominal, the reference
 # resistance, and the number of wires for the sensor (2 the default, 3, or 4)
 # with keyword args:
-#sensor = adafruit_max31865.MAX31865(spi, cs, rtd_nominal=100, ref_resistor=430.0, wires=2)
+# sensor = adafruit_max31865.MAX31865(spi, cs, rtd_nominal=100, ref_resistor=430.0, wires=2)
 
 # Main loop to print the temperature every second.
 while True:
     # Read temperature.
     temp = sensor.temperature
     # Print the value.
-    print('Temperature: {0:0.3f}C'.format(temp))
+    print("Temperature: {0:0.3f}C".format(temp))
     # Delay for a second.
     time.sleep(1.0)
